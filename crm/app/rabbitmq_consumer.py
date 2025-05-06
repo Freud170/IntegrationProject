5036f749f001337d66b979897c6e61eedab3be0d
noah <noahfeyder@gmail.com>
2025-05-06 14:11:18 +0200

    fix erp2

--- crm/app/rabbitmq_consumer.py
+++ crm/app/rabbitmq_consumer.py
@@ -5,9 +5,10 @@ from app import crud
 
 RABBITMQ_URL = "amqp://guest:guest@rabbitmq/"
 
-# Consumer für Bestellungen
 async def consume_order_updates():
-
+    """
+    Verarbeitet Nachrichten aus der Queue 'order_updates', um Bestellungen hinzuzufügen.
+    """
     connection = await aio_pika.connect_robust(RABBITMQ_URL)
     async with connection:
         channel = await connection.channel()
@@ -27,9 +28,11 @@ async def consume_order_updates():
                 except Exception as e:
                     print(f" [!] Error processing order: {e}")
 
-# Consumer für Bestellstatus-Updates
-async def consume_status_updates():
 
+async def consume_status_updates():
+    """
+    Verarbeitet Nachrichten aus der Queue 'order_status_updates', um den Status von Bestellungen zu aktualisieren.
+    """
     connection = await aio_pika.connect_robust(RABBITMQ_URL)
     async with connection:
         channel = await connection.channel()
@@ -49,7 +52,7 @@ async def consume_status_updates():
                 except Exception as e:
                     print(f" [!] Error processing status update: {e}")
 
-# Loop für die Consumer
+
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
     loop.create_task(consume_order_updates())
